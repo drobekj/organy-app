ba19db622e65319a21206f499438505c8394e041
drobekj <106076286+drobekj@users.noreply.github.com>
2026-08-31 20:41:15 +0200

    Adopt protected WhatsApp boundary: drizzle/0021_protected_whatsapp_phone.sql

--- drizzle/0021_protected_whatsapp_phone.sql
+++ drizzle/0021_protected_whatsapp_phone.sql
@@ -1,5 +1,14 @@
-ALTER TABLE "app_users" ADD COLUMN "whatsapp_phone_e164" text;
---> statement-breakpoint
-ALTER TABLE "app_users" ADD CONSTRAINT "app_users_whatsapp_phone_e164_valid" CHECK (
-  "whatsapp_phone_e164" is null or "whatsapp_phone_e164" ~ '^\+[1-9][0-9]{7,14}$'
-);
+ALTER TABLE "protected_account_actor_links"
+  ADD COLUMN "whatsapp_phone_e164" text,
+  ADD COLUMN "whatsapp_phone_confirmed_at" timestamp with time zone;
+
+ALTER TABLE "protected_account_actor_links"
+  ADD CONSTRAINT "protected_account_whatsapp_phone_state_valid"
+  CHECK (
+    ("whatsapp_phone_e164" IS NULL AND "whatsapp_phone_confirmed_at" IS NULL)
+    OR
+    (
+      "whatsapp_phone_e164" ~ '^\+[1-9][0-9]{7,14}$'
+      AND "whatsapp_phone_confirmed_at" IS NOT NULL
+    )
+  );
